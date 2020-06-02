b03a77ecc4f8caf257424e643de84ef2f77823e8
ducnhat99 <ducnhat220699@gmail.com>
2020-06-02 22:26:41 +0700

    nhat update

---
+++ QuanLyBanHang/DAL/Lop_DTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class Lop_DTO
+    {
+        public SqlConnection getConnect()
+        {
+            return new SqlConnection(@"Data Source=DESKTOP-G3PG01C\NHATNGUYEN;Initial Catalog=QLBH;Integrated Security=True");
+        }
+
+        public DataTable getTable()
+        {
+
+        }
+    }
+}
