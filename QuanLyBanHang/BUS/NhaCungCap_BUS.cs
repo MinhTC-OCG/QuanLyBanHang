75b0c0dc1112ce5dc8363d67fc090dc075a9745e
mb231999 <mb231999@github.com>
2020-06-03 18:31:48 +0700

    update

--- QuanLyBanHang/BUS/NhaCungCap_BUS.cs
+++ QuanLyBanHang/BUS/NhaCungCap_BUS.cs
@@ -22,26 +22,26 @@ namespace BUS
 
         public void InsertNhaCungCap(string mancc, string tenncc, string diachi, int sodt)
         {
-            string sql = "INSERT INTO Nhacungcap12 VALUES ('" + mancc + "',N'" + tenncc + "',N'" + diachi + "',N'" + sodt + "')";
+            string sql = "INSERT INTO NhaCungCap12 VALUES ('" + mancc + "',N'" + tenncc + "',N'" + diachi + "',N'" + sodt + "')";
             dal.ExecuteNonQuery(sql);
         }
 
 
         public void UpdateNhaCungCap(string mancc, string tenncc, string diachi, int sodt)
         {
-            string sql = "UPDATE Nhacungcap12 SET TenNCC = N'" + tenncc + "',diachi = N'" + diachi + "',sodt = N'" + sodt + "' WHERE mancc = '" + mancc + "'";
+            string sql = "UPDATE NhaCungCap12 SET TenNCC = N'" + tenncc + "',diachi = N'" + diachi + "',sodt = N'" + sodt + "' WHERE mancc = '" + mancc + "'";
             dal.ExecuteNonQuery(sql);
         }
 
         public void DeleteNhaCungCap(string mancc)
         {
-            string sql = "DELETE Nhacungcap12 WHERE mancc = '" + mancc + "'";
+            string sql = "DELETE NhaCungCap12 WHERE MaNCC = '" + mancc + "'";
             dal.ExecuteNonQuery(sql);
         }
 
         public DataTable SearchNhaCungCap(string mancc)
         {
-            string sql = "SELECT *FROM Nhacungcap12 WHERE mancc like '%" + mancc + "'";
+            string sql = "SELECT *FROM NhaCungCap12 WHERE MaNCC like '%" + mancc + "'";
             DataTable dt = new DataTable();
             dt = dal.getTable(sql);
             return dt;
