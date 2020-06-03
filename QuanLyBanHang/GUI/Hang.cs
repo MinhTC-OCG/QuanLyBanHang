edf2f6148c21ba60ba0fad7c1e8699064ffbb3cd
ducnhat99 <ducnhat220699@gmail.com>
2020-06-03 11:38:26 +0700

    .

--- QuanLyBanHang/GUI/Hang.cs
+++ QuanLyBanHang/GUI/Hang.cs
@@ -14,12 +14,96 @@ namespace GUI
     public partial class Hang : Form
     {
         Hang_BUS bus = new Hang_BUS();
-        Hang_DTO dto = new Hang_DTO();
+        Hang_DTO dto;
 
         DataTable dtHang, dtLoaiHang, dtNhaCungCap, dtTimKiemHang;
         public Hang()
         {
             InitializeComponent();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtMaH.Text == "")
+                    MessageBox.Show("Bạn chưa nhập mã loại, nhập lại!");
+                else if (txtTenH.Text == "")
+                    MessageBox.Show("Bạn chưa nhập tên hàng, nhập lại!");
+                else if (txtDonVT.Text == "")
+                    MessageBox.Show("Bạn chưa nhập đơn vị, nhập lại!");
+                else if (txtDonG.Text == "")
+                    MessageBox.Show("Bạn chưa nhập đơn giá, nhập lại!");
+                else if (txtSLC.Text == "")
+                    MessageBox.Show("Bạn chưa nhập tên loại, nhập lại!");
+                else
+                {
+                    int dem = 0;
+                    foreach (DataRow row in dtLoaiHang.Rows)
+                    {
+                        var check = row["MaH"].ToString().Trim();
+                        if (txtMaH.Text.Trim() == check)
+                        {
+                            dem++;
+                            break;
+                        }
+
+                    }
+                    if (dem == 0)
+                    {
+                        string mahang = txtMaH.Text;
+                        string tenhang = txtTenH.Text;
+                        string dongia = txtDonVT.Text;
+                        int dongia = txtDonG.Text;
+                        string maloai = cboMaL.SelectedValue.ToString();
+                        string mancc = cboMaNCC.SelectedValue.ToString();
+                        int soluongco = txtSLC.Text;
+                        bus.InsertHang(txtMaL.Text, txtTenL.Text, txtGhiChu.Text);
+                        MessageBox.Show("Thêm loại hàng thành công.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        LoadData();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Mã loại hàng đã tồn tại, nhập lại!", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Không thêm được loại hàng, thử lại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNhapLai_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
         }
 
         private void Hang_Load(object sender, EventArgs e)
@@ -57,5 +141,7 @@ namespace GUI
             (dgvHang.Columns["MaNCC"] as DataGridViewComboBoxColumn).DisplayMember = "TenNCC";
             (dgvHang.Columns["MaNCC"] as DataGridViewComboBoxColumn).ValueMember = "MaNCC";
         }
+
+
     }
 }
