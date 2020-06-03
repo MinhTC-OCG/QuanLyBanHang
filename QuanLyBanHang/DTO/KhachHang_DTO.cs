7eec872a5adcbae106d04fcb623443c9897fbafa
longld <49582581+longlaitien@github.com>
2020-06-03 11:36:23 +0700

    Tạo DTO: Khách hàng, Lịch sử giá

---
+++ QuanLyBanHang/DTO/KhachHang_DTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class KhachHang_DTO
+    {
+        String MaKhachHang_, HoTen_, DiaChi, SoDienThoai_;
+
+        public KhachHang_DTO(string maKhachHang_, string hoTen_, string diaChi, string soDienThoai_)
+        {
+            MaKhachHang_ = maKhachHang_;
+            HoTen_ = hoTen_;
+            DiaChi = diaChi;
+            SoDienThoai_ = soDienThoai_;
+        }
+
+        public string MaKhachHang_1 { get => MaKhachHang_; set => MaKhachHang_ = value; }
+        public string HoTen_1 { get => HoTen_; set => HoTen_ = value; }
+        public string DiaChi1 { get => DiaChi; set => DiaChi = value; }
+        public string SoDienThoai_1 { get => SoDienThoai_; set => SoDienThoai_ = value; }
+    }
+}
