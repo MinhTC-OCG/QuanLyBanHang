de5b082353a0dcb3afaadd89e07ffc0d15b53f93
ducnhat99 <ducnhat220699@gmail.com>
2020-06-03 13:33:50 +0700

    .....

--- QuanLyBanHang/GUI/Program.cs
+++ QuanLyBanHang/GUI/Program.cs
@@ -16,7 +16,7 @@ namespace GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TrinhDo());
+            Application.Run(new Hang());
         }
     }
 }
