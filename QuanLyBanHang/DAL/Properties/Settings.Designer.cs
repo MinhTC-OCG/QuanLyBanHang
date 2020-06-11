2ff98d9abb8f4ee63ce3d49853824d66567d0ce9
mb231999 <mb231999@github.com>
2020-06-11 11:53:49 +0700

    sod

--- QuanLyBanHang/DAL/Properties/Settings.Designer.cs
+++ QuanLyBanHang/DAL/Properties/Settings.Designer.cs
@@ -44,5 +44,15 @@ namespace DAL.Properties {
                 return ((string)(this["QLBHConnectionString1"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-KRH26DK\\MINH;Initial Catalog=QLBH;Integrated Security=True")]
+        public string QLBHConnectionStringMinh {
+            get {
+                return ((string)(this["QLBHConnectionStringMinh"]));
+            }
+        }
     }
 }
