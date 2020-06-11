d0457c39684371d2a9662533d115b6d9143e02e1
ngochuong2049 <66053080+ngochuong2049@users.noreply.github.com>
2020-06-11 09:54:38 +0700

    aaa

--- QuanLyBanHang/DAL/Lop_DAL.cs
+++ QuanLyBanHang/DAL/Lop_DAL.cs
@@ -12,9 +12,10 @@ namespace DAL
 
         public SqlConnection getConnect()
         {
-            return new SqlConnection(@"Data Source=DESKTOP-KRH26DK\MINH;Initial Catalog=QLBH;Integrated Security=True");
+            return new SqlConnection(@"Data Source=ARINAHASHIMOTO\SQLEXPRESS;Initial Catalog=QLBH;Integrated Security=True");
             //Data Source=DESKTOP-G3PG01C\NHATNGUYEN;Initial Catalog=QLBH;Integrated Security=True
             //Data Source=DESKTOP-KRH26DK\MINH;Initial Catalog=QLBH;Integrated Security=True
+            //Data Source=ARINAHASHIMOTO\SQLEXPRESS;Initial Catalog=QLBH;Integrated Security=True
         }
 
         public DataTable getTable(string sql)
